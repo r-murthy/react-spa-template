23a9a604f8383b9fb8aad61b97459e2a39011643
MIV_MMXX <rakeshkaranth@gmail.com>
2022-05-05 20:19:37 +0200

    initial commit

--- web/src/services/authsvc.js
+++ web/src/services/authsvc.js
@@ -13,12 +13,11 @@ import {
 } from "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyCn3VXwkmLvubI5TZytQNH1D8nut8FoQgY",
-  authDomain: "credit-7f47d.firebaseapp.com",
-  projectId: "credit-7f47d",
-  storageBucket: "credit-7f47d.appspot.com",
-  messagingSenderId: "486648757058",
-  appId: "1:486648757058:web:1232aa94de5f9be53926db",
+  apiKey: "AIzaSyCFE2UKkUlDpQRsge8tfEl66mRAYhsGKUY",
+  authDomain: "credit-bodi.firebaseapp.com",
+  projectId: "credit-bodi",
+  storageBucket: "credit-bodi.appspot.com",
+  appId: "1:501475336641:web:a26c5c0fffa001f2c48f40",
 };
 
 const firebaseApp = initializeApp(config);
